7131bb9c9db6f535c7416538b67a5e51d8036b1b
haroonabbasiii <abbasiharoon12@gmail.com>
2022-11-11 23:23:40 +0500

    number validator:  2nd and 3rd case

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function NumberValidator(Str) {
     var count = 0;
+    for(var i=0;Str[i]==" ";i++)
+    {
+    }
     if (Str[count] == "1") {
         ++count;
         if (Str[count] == " ") {
@@ -40,20 +43,69 @@ function NumberValidator(Str) {
                     }
                 }
             }
+            else if (Str[count] == "(") {
+                ++count;
+                if (Str[count] == "5") {
+                    ++count;
+                    if (Str[count] == "5") {
+                        ++count;
+                        if (Str[count] == "5") {
+                            ++count;
+                            if (Str[count] == ")") {
+                                ++count;
+                                if (Str[count] == " ") {
+                                    ++count;
+                                    if (Str[count] == "5") {
+                                        ++count;
+                                        if (Str[count] == "5") {
+                                            ++count;
+                                            if (Str[count] == "5") {
+                                                ++count;
+                                                if (Str[count] == "-") {
+                                                    ++count;
+                                                    if (Str[count] == "5") {
+                                                        ++count;
+                                                        if (Str[count] == "5") {
+                                                            ++count;
+                                                            if (Str[count] == "5") {
+                                                                ++count;
+                                                                if (Str[count] == "5") {
+                                                                    return true;
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
+    }
+    else if(Str=="5555555555")
+    {
+        return true;
+    }
+    else if(Str=="555-555-5555")
+    {
+        return true;
     }
     return false;
 }
 function Main_ra() {
     alert("The Number ");
     var Str = document.getElementById("Number").value;
-    
+
     if (NumberValidator(Str)) {
         document.getElementById("displaytext").innerText = "The Number is Correct";
         alert("The Number is Correct");
     }
-    else
-    {
+    else {
         document.getElementById("displaytext").innerText = "The Number is Not Correct";
         alert("The Number is Not Correct");
     }
