a74e19f090769ebbe9e75c8046b24294272ff265
haroonabbasiii <abbasiharoon12@gmail.com>
2022-11-11 21:34:23 +0500

    number validator:  1st case 1 555-555-5555

---
+++ script.js
@@ -0,0 +1,60 @@
+function NumberValidator(Str) {
+    var count = 0;
+    if (Str[count] == "1") {
+        ++count;
+        if (Str[count] == " ") {
+            ++count;
+            if (Str[count] == "5") {
+                ++count;
+                if (Str[count] == "5") {
+                    ++count;
+                    if (Str[count] == "5") {
+                        ++count;
+                        if (Str[count] == "-") {
+                            ++count;
+                            if (Str[count] == "5") {
+                                ++count;
+                                if (Str[count] == "5") {
+                                    ++count;
+                                    if (Str[count] == "5") {
+                                        ++count;
+                                        if (Str[count] == "-") {
+                                            ++count;
+                                            if (Str[count] == "5") {
+                                                ++count;
+                                                if (Str[count] == "5") {
+                                                    ++count;
+                                                    if (Str[count] == "5") {
+                                                        ++count;
+                                                        if (Str[count] == "5") {
+                                                            return true;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+function Main_ra() {
+    alert("The Number ");
+    var Str = document.getElementById("Number").value;
+    
+    if (NumberValidator(Str)) {
+        document.getElementById("displaytext").innerText = "The Number is Correct";
+        alert("The Number is Correct");
+    }
+    else
+    {
+        document.getElementById("displaytext").innerText = "The Number is Not Correct";
+        alert("The Number is Not Correct");
+    }
+}
